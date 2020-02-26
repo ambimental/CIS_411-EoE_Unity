001e354064f066aff85869ed0e8c6898c656d2fa
TylerKrow <60166269+TylerKrow@users.noreply.github.com>
2020-02-26 14:23:22 -0800

    Update Deck-Refactor

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -1,24 +1,40 @@
-﻿using System.Collections;
+﻿/*
+ *  @class      Deck
+ *  @purpose    Hold/identify player decks and cards within decks
+ *  
+ *  @author     John Georgvich, previous CIS411 group
+ *  @date       2020/01/22
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Deck
 {
 
-    private string deckId; //id of the deck
-    private List<Card> cards; //stores the cards
+    //  deck id class variable
+    private string deckId;
+    //  List<Card> object used to store all cards in deck
+    private List<Card> cards;
 
-    //accessors and mutators
+    /// <summary>
+    /// ACCESSOR/MUTATOR BLOCK
+    /// </summary>
     public string DeckId { get { return deckId; } set { deckId = value; } }
     public List<Card> Cards { get { return cards; } set { cards = value; } }
 
-    //default constructor
+    /*
+     *  @name       Deck()
+     *  @purpose    Default class constructor for Deck
+     */
     public Deck()
     {
         DeckId = "";
         Cards = new List<Card>();
     }
 
+<<<<<<< HEAD
     // Use this for initialization
     void Start()
     {
@@ -31,5 +47,23 @@ public class Deck
     {
 
     }
+=======
+	/*
+     *  @name       Start()
+     *  @purpose    Used by Unity engine for object initialization
+     */
+	void Start (){
+        DeckId = "";
+        Cards = new List<Card>();
+    }
+	
+	/*
+     *  @name       Update()
+     *  @purpose    Updates object every frame
+     */
+	void Update () {
+		
+	}
+>>>>>>> dev
 }
 
